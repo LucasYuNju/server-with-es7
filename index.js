46178de442439da63b8a4758c4d22796f0c9b5b2
LucasYuNju <LucasYuNju@gmail.com>
2016-11-30 15:29:44 +0800

    Add http server

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-(async () => {
-    console.log("Hello, es6");
-})();
+import httpServer from "./lib/http/server";
+
+const port = 8000;
+httpServer.listen(8000, () => {
+    console.log(`Server is listening at port ${port}...`);
+});
