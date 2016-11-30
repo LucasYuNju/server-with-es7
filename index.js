ba1ddb6be4a357b7fadb153f3c50803d6052558b
LucasYuNju <LucasYuNju@gmail.com>
2016-11-30 15:39:37 +0800

    Add app.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import httpServer from "./lib/http/server";
+import app from "./lib/app";
+
+app.attach(httpServer);
+// equivalent httpServer.on("request", app);
 
 const port = 8000;
 httpServer.listen(8000, () => {
